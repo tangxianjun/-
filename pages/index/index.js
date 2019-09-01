8f30efa9fa9dbaa68cd1e7fac7dd7dcf659c8c5e
tangxianjun <1125288167@qq.com>
2019-09-01 13:22:05 +0800

    modified:   app.js 	modified:   app.json 	modified:   app.wxss 	new file:   "pages/images/\345\256\266\347\224\250\347\224\265\345\231\250@16_16.png" 	new file:   "pages/images/\345\256\266\347\224\250\347\224\265\345\231\250@32_32.png" 	new file:   "pages/images/\346\220\234\347\264\242@hdpi.png" 	new file:   "pages/images/\346\225\231\346\235\220\344\271\246\347\261\215@32_32.png" 	new file:   "pages/images/\346\225\260\347\240\201\344\272\247\345\223\201@32_32.png" 	new file:   "pages/images/\347\224\237\346\264\273\347\224\250\345\223\201@32_32.png" 	new file:   "pages/images/\347\276\216\345\246\206\346\227\245\345\214\226@32_32.png" 	new file:   "pages/images/\350\241\243\347\211\251\351\236\213\345\270\275@32_32.png" 	new file:   "pages/images/\351\246\226\351\241\265banner@hdpi.png" 	modified:   pages/index/index.js 	modified:   pages/index/index.wxml 	modified:   pages/index/index.wxss 	new file:   pages/index/search/search.js 	new file:   pages/index/search/search.json 	new file:   pages/index/search/search.wxml 	new file:   pages/index/search/search.wxss 	modified:   project.config.json 	new file:   sitemap.json

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,149 @@
 const app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
+    indicatorDots: true, 
+    autoplay:true,
+    userInfo:{},
+    hasUserInfo:false,
+    classify_top:[
+      {
+        title:"图书教材",
+        url:"../images/教材书籍@32_32.png"
+      },
+      {
+        title: "数码产品",
+        url: "../images/数码产品@32_32.png"
+      },
+      {
+        title: "衣物鞋帽",
+        url: "../images/衣物鞋帽@32_32.png"
+      }
+      
+    ],
+    classify_foot:[
+      {
+        title: "生活用品",
+        url: "../images/生活用品@32_32.png"
+      },
+      {
+        title: "家用电器",
+        url: "../images/家用电器@32_32.png"
+      },
+      {
+        title: "美妆日化",
+        url: "../images/美妆日化@32_32.png"
+      }
+    ],
+    note:[],
     canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    
+    // 获取商品列表
+    
+    var that = this;
+    wx.request({
+      url: 'http://127.0.0.1/php/goodsinfo.php', //仅为示例，并非真实的接口地址
+      method:"POST",
+      data: {
+        status:'1'
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success:function(res){
+        // console.log(res.data);
+        that.setData({
+          note:res.data
+        });
+        // console.log(that.data.note);
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+    wx.getSetting({
+      success(res) {
+        console.log(res.authSetting)
+        console.log(!res.authSetting['scope.userInfo'])
+        // res.authSetting = {
+        //   "scope.userInfo": true,
+        //   "scope.userLocation": true
+        // }
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success() {
+              wx.showToast({
+                title: '授权成功！'
+              })
+            }
           })
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      }
     })
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  navigator:function(){
+   
   }
+
 })
